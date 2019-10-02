580ee7a35b550e2d8c567bcf7080e1a05fb5abd8
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2019-10-02 15:10:00 +1000

    added pylintrc file and fix linting

--- lambda_functions/manage_ec2/handler.py
+++ lambda_functions/manage_ec2/handler.py
@@ -1,10 +1,10 @@
 import json
 import logging
-import boto3
-from botocore.exceptions import ClientError
 import os
 from datetime import datetime
 from dateutil.tz import tz
+import boto3
+from botocore.exceptions import ClientError
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
@@ -30,18 +30,18 @@ def handler(event, context):
     It will then power on / off each instance based on SCHEDULE_START_TIME and SCHEDULE_STOP_TIME provided.
     """
 
-    LOG.info("Received event: " + json.dumps(event, indent=2))
+    LOG.info("Received event: %s", json.dumps(event, indent=2))
 
     ec2 = boto3.client('ec2', region_name=REGION)
 
     now = datetime.now(tz=TIME_ZONE).time()
-    LOG.info('Current time is {0}'.format(now))
+    LOG.info('Current time is %s', now)
 
     start_time = os.environ.get('SCHEDULE_START_TIME')
     start_time = datetime.strptime(start_time, '%H:%M').time()
     stop_time = os.environ.get('SCHEDULE_STOP_TIME')
     stop_time = datetime.strptime(stop_time, '%H:%M').time()
-    LOG.info("Start Time: {0}, Stop Time: {1}".format(start_time, stop_time))
+    LOG.info("Start Time: %s, Stop Time: %s", start_time, stop_time)
 
     instances = [
         {
@@ -51,21 +51,21 @@ def handler(event, context):
     ]
 
     instance_ids = []
-    ec2Reservations = ec2.describe_instances(Filters=instances)['Reservations']
+    ec2_reservations = ec2.describe_instances(Filters=instances)['Reservations']
 
-    for reservation in ec2Reservations:
-        ec2Instances = reservation['Instances']
-        for instance in ec2Instances:
+    for reservation in ec2_reservations:
+        ec2_instances = reservation['Instances']
+        for instance in ec2_instances:
             instance_ids.append(instance['InstanceId'])
 
     if _time_in_range(start_time, stop_time, now):
-        LOG.info("Starting instance: {}".format(','.join(instance_ids)))
+        LOG.info("Starting instance: %s", (','.join(instance_ids)))
         try:
             ec2.start_instances(InstanceIds=instance_ids)
         except ClientError as e:
             LOG.error(e)
     else:
-        LOG.info("Stopping instance: {}".format(','.join(instance_ids)))
+        LOG.info("Stopping instance: %s", (','.join(instance_ids)))
         try:
             ec2.stop_instances(InstanceIds=instance_ids)
         except ClientError as e:
